0ae8151d3f3f4b76e9e52bc026758a1eec42ee24
Filipe Petter <fgpetter@gmail.com>
2026-03-03 15:00:21 -0300

    chore: minor fixes

--- app/Enums/OcorrenciaStatus.php
+++ app/Enums/OcorrenciaStatus.php
@@ -4,9 +4,27 @@ namespace App\Enums;
 
 enum OcorrenciaStatus: string
 {
+    /**
+     * @var string
+     * Importado da planilha ou cadastrado manualmente
+     */
     case Aberto = 'aberto';
+    /**
+     * @var string
+     * Quando o admin libera o atendimento para ficar visível para o prestador
+     */
     case Andamento = 'andamento';
+    /**
+     * @var string
+     * Quando o campo concluido_prestador estiver preenchido, para que o admin
+     * possa revisar os dados do atendimento
+     */
     case Revisar = 'revisar';
+    /**
+     * @var string
+     * após o admin revisar e marcar como concluido, adicionar na tabela a coluna 
+     * "concluido_por" com o ID do admin que marcou como concluido
+     */
     case Concluido = 'concluido';
 
     public function label(): string
