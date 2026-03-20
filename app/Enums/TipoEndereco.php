b3453648933fcb5ed31820ae63f556eb2d8d4555
Filipe Petter <fgpetter@gmail.com>
2026-03-20 11:17:13 -0300

    FIX: ajusta enum de enderecos

--- app/Enums/TipoEndereco.php
+++ app/Enums/TipoEndereco.php
@@ -4,12 +4,26 @@ namespace App\Enums;
 
 enum TipoEndereco: string
 {
-    case Agencia = 'agencia';
+    case AGENCIA = 'AGENCIA';
+    case DATAC = 'DATAC';
+    case DG = 'DG';
+    case EMPRESAS = 'EMPRESAS';
+    case PA = 'PA';
+    case PA_PM = 'PA PM';
+    case PAE = 'PAE';
+    case SUREG = 'SUREG';
 
     public function label(): string
     {
         return match ($this) {
-            self::Agencia => 'Agência',
+            self::AGENCIA => 'AGENCIA',
+            self::DATAC => 'DATAC',
+            self::DG => 'DG',
+            self::EMPRESAS => 'EMPRESAS',
+            self::PA => 'PA',
+            self::PA_PM => 'PA PM',
+            self::PAE => 'PAE',
+            self::SUREG => 'SUREG',
         };
     }
 
